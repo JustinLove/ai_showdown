302596c2f0be20f287c21ae80cebe4fbddc00b28
Justin Love <git@JustinLove.name>
2015-04-22 08:04:15 -0500

    platoon templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,25 @@ var modPath = '../../server_mods/com.wondible.pa.ai_showdown/'
 var stream = 'stable'
 var media = require('./lib/path').media(stream)
 
+var ais = {
+  ais: [
+    {
+      path: 'ai/vanilla',
+      rule_postfix: '',
+      file_postfix: '',
+      name_prefix: '',
+      unittype: 'Bot',
+    },
+    {
+      path: 'ai/quellar',
+      rule_postfix: '_Quellar',
+      file_postfix: '_Quellar',
+      name_prefix: 'Quellar - ',
+      unittype: 'Tank',
+    },
+  ]
+}
+
 module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
@@ -42,46 +61,22 @@ module.exports = function(grunt) {
       },
     },
     clean: ['pa', 'ai/vanilla', modPath],
-    // copy files from PA, transform, and put into mod
-    proc: {
-      // form 1: just the relative path, media src is assumed
-      adv_comfab: {
-        targets: [
-          'pa/units/land/fabrication_bot_combat_adv/fabrication_bot_combat_adv.json'
-        ],
-        process: function(spec) {
-          spec.build_metal_cost = 1000
-        }
-      },
-      // form 2: munge one or more specs from PA into one in the mod
-      antinuke: {
-        src: [
-          'pa/units/land/anti_nuke_launcher/anti_nuke_launcher.json',
-          'pa/units/land/anti_nuke_launcher/anti_nuke_launcher_ammo.json'
-        ],
-        cwd: media,
-        dest: 'pa/units/land/anti_nuke_launcher/anti_nuke_launcher.json',
-        process: function(spec, ammo) {
-          spec.factory.default_ammo = [ spec.factory.initial_build_spec ]
-          spec.build_metal_cost += ammo.build_metal_cost
-          return spec
-        }
-      }
-    }
+    platoons: {},
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerMultiTask('proc', 'Process unit files into the mod', function() {
-    if (this.data.targets) {
-      var specs = spec.copyPairs(grunt, this.data.targets, media)
-      spec.copyUnitFiles(grunt, specs, this.data.process)
-    } else {
-      var specs = this.filesSrc.map(function(s) {return grunt.file.readJSON(media + s)})
-      var out = this.data.process.apply(this, specs)
-      grunt.file.write(this.data.dest, JSON.stringify(out, null, 2))
-    }
+  grunt.registerTask('platoons', 'Process unit files into the mod', function() {
+    out = {}
+    ais.ais.forEach(function(ai) {
+      platoons = grunt.file.readJSON(ai.path + '/platoon_templates.json').platoon_templates
+      Object.keys(platoons).forEach(function(name) {
+        out[name + ai.rule_postfix] = platoons[name]
+      })
+    })
+    grunt.file.write('pa/ai/platoon_templates.json',
+                     JSON.stringify({platoon_templates: out}, null, 2))
   })
 
   // Default task(s).
